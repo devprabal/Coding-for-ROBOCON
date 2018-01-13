c33b802f8a8018a17f4e5140cb638e3b99897546
devprabal <35391821+devprabal@users.noreply.github.com>
2018-01-13 23:08:48 +0530

    Add files via upload

---
+++ reed_switch_and_micro_switch/reed_switch_and_micro_switch.ino
@@ -0,0 +1,39 @@
+const int pinSwitch = 12; //Pin Reed
+
+const int pinLed = 9; //Pin LED
+
+int StatoSwitch = 0;
+
+void setup()
+
+{
+
+pinMode(pinLed, OUTPUT); //Imposto i PIN
+
+pinMode(pinSwitch, INPUT);
+
+}
+
+void loop()
+
+{
+
+StatoSwitch = digitalRead(pinSwitch); //Leggo il valore del Reed
+
+if (StatoSwitch == HIGH)
+
+{
+
+digitalWrite(pinLed, HIGH);
+
+}
+
+else
+
+{
+
+digitalWrite(pinLed, LOW);
+
+}
+
+}
