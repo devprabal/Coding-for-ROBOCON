f87536720393e71e8eb10d648fbacc60ab721eda
devprabal <35391821+devprabal@users.noreply.github.com>
2018-01-13 13:54:58 +0530

    Add files via upload

---
+++ Laser_and_Detector.ino
@@ -0,0 +1,34 @@
+/*
+
+Code for KY-008 Laser and Laser Detector
+by eLab Peers (C) 2014.
+
+Visit us at:
+http://www.elabpeers.com
+
+All rights reserved.
+
+Wiring:
+1.  VCC to 5V on Arduino board
+2.  GND to GND on Arduino board
+3.  S on KY-008 Laser to Pin 6 on Arduino board
+4.  OUT on Laser Dector to Pin 7 on Arduino board
+
+*/
+
+int Laser = 6;
+int Detector = 7;
+
+void setup()
+{ 
+  Serial.begin (9600);
+  pinMode(Laser, OUTPUT);
+  pinMode(Detector, INPUT);
+}
+
+void loop()
+{
+  digitalWrite(Laser, HIGH);
+  boolean val = digitalRead(Detector);
+  Serial.println(val);
+}  
