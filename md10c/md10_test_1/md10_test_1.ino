4e0aec75b2c70da5d0483130c2cb0064165949d7
devprabal <35391821+devprabal@users.noreply.github.com>
2018-01-14 15:50:51 +0530

    Add files via upload

---
+++ md10c/md10_test_1/md10_test_1.ino
@@ -0,0 +1,24 @@
+//#define dir 2
+#define pwmPin 3
+
+void setup() {
+  // put your setup code here, to run once:
+//  pinMode(dir,OUTPUT);
+  pinMode(pwmPin, OUTPUT);
+//  digitalWrite(dir, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+//  for(pwm_value=0;pwm_value<256;pwm_value++)
+//  {
+//    analogWrite(pwmPin,pwm_value); //increase PWM in every 0.1 sec
+//  }
+  
+    digitalWrite(pwmPin,HIGH); 
+    delay(1); 
+    digitalWrite(pwmPin,LOW); 
+    delay(1);
+ 
+}
