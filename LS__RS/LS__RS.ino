4dc0895aa49feeeb78e2176e47341f59d082db8c
ShrutiPandey01 <35392986+ShrutiPandey01@users.noreply.github.com>
2018-01-15 18:08:48 +0530

    Add files via upload

---
+++ LS__RS/LS__RS.ino
@@ -0,0 +1,34 @@
+int LS;
+int RS;
+int BS;
+int FS;
+int LineSensor;
+
+
+void setup() {
+ 
+ Serial.begin(9600);
+
+}
+
+void loop() {
+  LS = Serial.read();
+  RS = Serial.read();
+  LS = Serial.read();
+  RS = Serial.read();
+  LineSensor = Serial.read();
+
+  if( LS ==0 && RS==0 && BS == 1 && FS ==1 && LineSensor ==1){
+    Serial.println("FORWARD");
+  }
+
+  if( LS == 1 && RS==1 && BS==1 && FS == 1 && LineSensor ==1){
+    Serial.println("STOP");
+    Serial.println("TakeShuttle");
+    Serial.println("DetectColour");
+  }
+  if( LS== 1 && RS ==1 && BS == 0 && FS == 0 && LineSensor ==1){
+    Serial.println("SLOW");
+  }
+  
+}
