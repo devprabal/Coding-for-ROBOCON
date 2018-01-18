a731e5c1c9fef2a80a4003c40883831b6c5a0048
devprabal <35391821+devprabal@users.noreply.github.com>
2018-01-18 20:26:37 +0530

    Update sketch_jan17a_virtual_LF_improve.ino

--- lineFollower-13jan2018/Virtual line follower/sketch_jan16a/sketch_jan17a_virtual_LF_improve/sketch_jan17a_virtual_LF_improve.ino
+++ lineFollower-13jan2018/Virtual line follower/sketch_jan16a/sketch_jan17a_virtual_LF_improve/sketch_jan17a_virtual_LF_improve.ino
@@ -1,7 +1,8 @@
   char tz1color = 'b';
   char tz2color = 'w';
   char tz3color = 'g';
-  bool cross=false;
+  char cross='0';
+  char flag = '0';
   void back()
   {
     Serial.println("Back");
@@ -16,13 +17,13 @@
   }
   int isAligned()
   {
-    int al=0;
+    char al='a';
     while(1)
     {
       Serial.println("Press 1 for allign, else 0 ");
       while(Serial.available()<=0);
       al=Serial.read();
-        if(al==1){
+        if(al=='1'){
         Serial.println("bot aligned");
         break;
         }
@@ -30,17 +31,17 @@
           Serial.println("Bot not aligned");
         }
     }
-    return al;
+    return (int)al;
   }
-  bool takeShuttle()
+  char takeShuttle()
   {
-    bool st =0;
+    char st ='0';
     while(1)
     {
       Serial.println("Press 1 for Shuttle available");
       while(Serial.available()<=0);
       st = Serial.read();
-      if(st){
+      if(st == '1'){
       Serial.println("shuttle availble");
       break;
       }
@@ -54,7 +55,7 @@
   char detectColor()
   {
       char c;
-      while(1)
+     // while(1)
       {
         Serial.println("Detect color....\nENter color...\n'w'-white\n'b'-black\n'g'-gold");
         while(Serial.available()<=0);
@@ -67,13 +68,10 @@
         Serial.println("black detected");
         return 'b';
       }
-      else if (c=='g'){
+      else //if (c=='g')
+      {
         Serial.println("gold detected");
         return 'g';
-      }
-      else
-      {
-        Serial.println("invalid color detected");
       }
      }  
   }
@@ -81,15 +79,15 @@
   {
     Serial.println("Stop");
   }
-  int throww()
+  char throww()
   {
-    int th = 0;
+    char th = '0';
     while(1)
     {
       Serial.println("press 1 for positive feedback or else 0");
       while(Serial.available()<=0);
       th = Serial.read();
-      if(th)
+      if(th == '1')
       {
        break;
       }
@@ -98,36 +96,41 @@
   }
   void tz1() // throws at cross_no 2
   {
-    int cross_no = 0; // to count no. of crosses
-    int cross = 0; // to detect a cross
+    char cross_no = '0'; // to count no. of crosses
+    char cross = '0'; // to detect a cross
     
   
     while(1)
     {
-      int prev = 0;
-      int curr = 0;
+      char prev = '0';
+      char curr = '0';
       right();
       prev = curr;
       Serial.println("Press 0 for no cross, 1 for a cross");
       while(Serial.available()<=0);
       cross = Serial.read();
       curr = cross;
+      if (curr == prev)
+      {
+        right();
+      }
       if (curr != prev)
       {
-        cross_no++;
+        int(cross_no++);
+        right();
       }
-      if (cross_no == 2)
+      if (cross_no == '2')
       {
       stopp();
-      int feedback = throww();
+      char feedback = throww();
       break;
       }
     }
   //for going back 
     while(1)
     {
-      int prev = 0;
-      int curr = 0;
+      char prev = '0';
+      char curr = '0';
       left();
       prev = curr;
       Serial.println("Press 0 for no cross, 1 for a cross");
@@ -136,14 +139,15 @@
       curr = cross;
       if (curr != prev)
       {
-        cross_no--;
+        int(cross_no--);
       }
-      if (cross_no == 0)
+      if (cross_no == '0')
       {
       stopp();
       break;
       }
-    } 
+    }
+    
   }
   
   void tz2()
@@ -179,40 +183,47 @@
 //    }
 //    }
     tz1(); 
+    flag = '1'; 
   }
   
   void tz3() // throws at cross_no 5
   {
-    int cross_no = 0; // to count no. of crosses
-    int cross = 0; // to detect a cross
+    
+    char cross_no = '0'; // to count no. of crosses
+    char cross = '0'; // to detect a cross
     
   
     while(1)
     {
-      int prev = 0;
-      int curr = 0;
+      char prev = '0';
+      char curr = '0';
       right();
       prev = curr;
       Serial.println("Press 0 for no cross, 1 for a cross");
       while(Serial.available()<=0);
       cross = Serial.read();
       curr = cross;
+      if (curr == prev)
+      {
+        right();
+      }
       if (curr != prev)
       {
-        cross_no++;
+        int(cross_no++);
+        right();
       }
-      if (cross_no == 5)
+      if (cross_no == '5')
       {
       stopp();
-      int feedback = throww();
+      char feedback = throww();
       break;
       }
     }
   //for going back 
     while(1)
     {
-      int prev = 0;
-      int curr = 0;
+      char prev = '0';
+      char curr = '0';
       left();
       prev = curr;
       Serial.println("Press 0 for no cross, 1 for a cross");
@@ -221,9 +232,9 @@
       curr = cross;
       if (curr != prev)
       {
-        cross_no--;
+        int(cross_no--);
       }
-      if (cross_no == 0)
+      if (cross_no == '0')
       {
       stopp();
       break;
@@ -241,31 +252,38 @@
   void loop() 
   {
     // put your main code here, to run repeatedly:
-    int cross = 0;
+    char cross = 0;
+    
+    //back();
     Serial.flush();
     while(1)
     {
-    Serial.println("\nPress 0 for not cross, 1 for cross  ");
-    while(Serial.available()<=0);
-    cross = Serial.read();
-    
-    if(cross == 0)
-     {
-      back();
-     }
-     else
-     {
-        break;
-     }
+      Serial.println("\nPress 0 for not cross, 1 for cross  ");
+      while(Serial.available()<=0);
+      cross = Serial.read();
+      
+      if(cross == '0')
+       {
+        if(flag == '0') 
+        back();
+        else
+        {
+          break;
+        }
+       }
+       else
+       {
+          break;
+       }
     }
   stopp();
-  delay(1000);
+  //delay(1000);
   
   if(isAligned()=='1')
   {
-    bool shuttle_status = takeShuttle();
+    char shuttle_status = takeShuttle();
     char shuttle_color = detectColor();
-    if (shuttle_status){
+    if (shuttle_status == '1'){
       if(shuttle_color == tz1color)
       {
         tz1();
