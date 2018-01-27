e28c69efaded0fd950dc7e47950e1f4840d585f9
devprabal <35391821+devprabal@users.noreply.github.com>
2018-01-27 18:56:50 +0530

    Add files via upload

---
+++ mecanumWheels_selfTest/mecanumWheels_selfTest.ino
@@ -0,0 +1,118 @@
+// direction Pins
+#define dirPinFR 7
+#define dirPinFL 8
+#define dirPinRR 4
+#define dirPinRL 2
+// pwm Pins
+#define pwmPinFR 6
+#define pwmPinFL 9
+#define pwmPinRL 3
+#define pwmPinRR 5
+void setDirection(bool FR, bool FL, bool RL, bool RR, int speedValue)
+{
+  digitalWrite(dirPinFR, FR);
+  digitalWrite(dirPinFL, FL);
+  digitalWrite(dirPinRL, RL);
+  digitalWrite(dirPinRR, RR);
+  for(int i = 0; i < speedValue; i+=2)
+  {
+    analogWrite(pwmPinFR, i);
+    analogWrite(pwmPinFL, i);
+    analogWrite(pwmPinRL, i);
+    analogWrite(pwmPinRR, i);
+  }
+  
+}
+void Movement(int speedValue)
+{
+    analogWrite(pwmPinFR, speedValue);
+    analogWrite(pwmPinFL, speedValue);
+    analogWrite(pwmPinRR, speedValue);
+    analogWrite(pwmPinRL, speedValue);
+}
+void ease_out(speedValue)
+{
+  for(int i = speedValue; i >= 0; i-=2)
+  {
+    analogWrite(pwmPinFR, i);
+    analogWrite(pwmPinFL, i);
+    analogWrite(pwmPinRL, i);
+    analogWrite(pwmPinRR, i);
+  }
+  delay(5000);
+}
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(dirPinFR, OUTPUT);
+  pinMode(dirPinFL, OUTPUT);
+  pinMode(dirPinRR, OUTPUT);
+  pinMode(dirPinRL, OUTPUT);
+  pinMode(pwmPinFR, OUTPUT);
+  pinMode(pwmPinFL, OUTPUT);
+  pinMode(pwmPinRR, OUTPUT);
+  pinMode(pwmPinRL, OUTPUT);
+  
+}
+
+void loop() {
+  // Forward direction
+  setDirection(1,1,1,1,150);//when button pressed (do once)
+  while(millis()<5000)
+  {
+    Movement(150);//keep on going till button is pressed (repeatedly)
+  }
+  
+  ease_out(150);//when button is released (do once)
+  // Backward direction
+  setDirection(0,0,0,0,150);
+  while(millis()<15000)
+  {
+    Movement(150);//keep on going till button is pressed (repeatedly)
+  }
+  ease_out(150);
+  // Left direction
+  setDirection(1,0,1,0,150);
+  while(millis()<25000)
+  {
+    Movement(150);//keep on going till button is pressed (repeatedly)
+  }
+  ease_out(150);
+  // Right direction
+  setDirection(0,1,0,1,150);
+  while(millis()<35000)
+  {
+    Movement(150);//keep on going till button is pressed (repeatedly)
+  }
+  ease_out(150);
+  // clockwise turning
+  setDirection(0,1,1,0,150);
+  while(millis()<45000)
+  {
+    Movement(150);//keep on going till button is pressed (repeatedly)
+  }
+  ease_out(150);
+  // anticlockwise turning
+  setDirection(0,1,1,0,150);
+  while(millis()<55000)
+  {
+    Movement(150);//keep on going till button is pressed (repeatedly)
+  }
+  ease_out(150);
+  //////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////DIAGONAL/////////////////////////////////////////////////
+  analogWrite(pwmPinFL,0);
+  analogWrite(pwmPinRR,0);
+  digitalWrite(dirPinFR,LOW);
+  digitalWrite(dirPinRL,LOW);
+  for(int i=0; i<150; i+=2)
+  {
+    analogWrite(pwmPinFR, i);
+    analogWrite(pwmPinRL, i);
+  }
+  for(int i=150; i>150; i-=2)
+  {
+    analogWrite(pwmPinFR, i);
+    analogWrite(pwmPinRL, i);
+  }
+  delay(2000);
+}
---
+++ xbee_receiver/xbee_receiver.ino
@@ -0,0 +1,76 @@
+#include <Servo.h>
+Servo myservo;
+ 
+int pos=0;
+
+//Variables
+bool started= false;//True: Message is strated
+bool ended   = false;//True: Message is finished 
+char incomingByte ; //Variable to store the incoming byte
+char msg[3];    //Message - array from 0 to 2 (3 values - PWM - e.g. 240)
+byte index;     //Index of array
+
+void setup() {
+
+  //Start the serial communication
+  Serial.begin(9600); //Baud rate must be the same as is on xBee module
+  
+  myservo.attach(9);
+}
+
+void loop() {
+  
+  while (Serial.available()>0){
+    //Read the incoming byte
+
+    incomingByte = Serial.read();
+    //Start the message when the '<' symbol is received
+    if(incomingByte == '<')
+    {
+     started = true;
+     index = 0;
+     msg[index] = '\0'; // Throw away any incomplete packet
+   }
+   //End the message when the '>' symbol is received
+   else if(incomingByte == '>')
+   {
+     ended = true;
+     break; // Done reading - exit from while loop!
+   }
+   //Read the message!
+   else
+   {
+     if(index < 4) // Make sure there is room
+     {
+       msg[index] = incomingByte; // Add char to array
+       index++;
+       msg[index] = '\0'; // Add NULL to end
+     }
+   }
+ }
+ 
+ if(started && ended)
+ 
+  {
+   int pos = atoi(msg);
+   
+   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+   
+   //Serial.println(value); //Only for debugging
+   index = 0;
+   msg[index] = '\0';
+   started = false;
+   ended = false;
+ }
+
+}
+ 
+
