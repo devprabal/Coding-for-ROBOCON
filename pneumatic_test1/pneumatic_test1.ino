2172c81fb8915a719ff91aaae3bda308dfa71276
devprabal <35391821+devprabal@users.noreply.github.com>
2018-02-01 15:43:54 +0530

    Add files via upload

---
+++ pneumatic_test1/pneumatic_test1.ino
@@ -0,0 +1,17 @@
+#define solA 3
+#define solB 4
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(solA, OUTPUT);
+  pinMode(solB, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(solA, HIGH);
+digitalWrite(solB, LOW);
+delay(2500);
+digitalWrite(solA, LOW);
+digitalWrite(solB, HIGH);
+
+}
