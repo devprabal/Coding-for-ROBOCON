352b22e7746ca8da4235467a0d0cde2b8341997a
Ayush <goyal.ayush55@gmail.com>
2018-02-10 16:22:11 +0530

    Added Gripper with IR code
    for releasing it in an angle

---
+++ Gripper with IR/sketch_feb10a.ino
@@ -0,0 +1,46 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com> 
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/ 
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+int irPin = 7;
+int prev=0,a;
+int count  = 0;
+ 
+void setup() 
+{ 
+  pinMode(irPin,INPUT);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+   myservo.write(41);
+   Serial.begin(9600);
+} 
+ 
+void loop() 
+{
+  prev = a;
+  a = digitalRead(irPin);
+  
+ // Serial.println(a);
+  if (prev!=a && a==HIGH){
+    count++;
+  }
+  Serial.println(count);
+  if (a == HIGH && count==6)
+  {
+    myservo.write(41);
+    delay(5000);
+    count = 0;
+  }
+  else{
+    myservo.write(65);
+  }
+} 
+
