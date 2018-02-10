145fd0a4fe200a28e423fb909cb4cb341082119c
dev p <ghjxvnjd@gmail.com>
2018-02-10 11:44:18 +0530

    for throwing

--- armRotationTest_with_IR/armRotationTest_with_IR.ino
+++ armRotationTest_with_IR/armRotationTest_with_IR.ino
@@ -1,40 +1,56 @@
-#include <Servo.h> 
+#include <Servo.h>
 #define IR 2
-#define dir 3
-#define pwmPin 5
- 
-Servo myservo;  // create servo object to control a servo 
-                // twelve servo objects can be created on most boards
- 
-int pos = 0;    // variable to store the servo position 
+#define dirPin 3 // motor driver
+#define pwmPin 5 // motor driver
 
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int open_position = 45;    // variable to store the servo position
+int close_position = 0;    // variable to store the servo position
+int count_rotation = 0;
+int speed_arm = 60; // 255 is maximum
 void setup() {
   // put your setup code here, to run once:
   pinMode(IR, INPUT);
-  pinMode(dir, OUTPUT);
+  pinMode(dirPin, OUTPUT);
   pinMode(pwmPin, OUTPUT);
-  digitalWrite(dir, HIGH);
+  digitalWrite(dirPin, HIGH);
   myservo.attach(9);
+  myservo.write(close_position);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(IR))
+  myservo.write(close_position);
+  digitalWrite(dirPin, HIGH);
+  analogWrite(pwmPin, speed_arm);
+  if (digitalRead(IR) == 1)
   {
-    //digitalWrite(pwmPin, LOW);
-    for (pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees
-    { // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
+    count_rotation ++;
+    if (count_rotation == 6)
+    {
+      myservo.write(open_position);
+      delay(15);
+      myservo.write(close_position);
+      /* arm stopping mechanism
+        if(digitalRead(IR)==1)
+        {
+        digitalWrite(dirPin, LOW);
+        analogWrite(pwmPin, speed_arm/2);
+        delay(10);
+        digitalWrite(dirPin, HIGH);
+        analogWrite(pwmPin, speed_arm/2);
+        delay(10);
+        }
+      */
     }
+
   }
   else
   {
-    //digitalWrite(pwmPin, HIGH);
-    for (pos = 90; pos >= 90; pos -= 1) // goes from 0 degrees to 180 degrees
-    { // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-    }
+    myservo.write(close_position);
+    delay(15);
   }
 }
+
